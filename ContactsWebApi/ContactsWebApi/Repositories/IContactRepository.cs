68cbdaab1bfc38ddcf00120cc3e33af7cb7a6d99
pekkamakkonen <pek.makkonen@gmail.com>
2018-05-24 14:54:03 +0300

    Contacs api CRUD operations added

---
+++ ContactsWebApi/ContactsWebApi/Repositories/IContactRepository.cs
@@ -0,0 +1,17 @@
+﻿using ContactsWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsWebApi.Repositories
+{
+    public interface IContactRepository
+    {
+        List<Contact> Get();
+        Contact Get(int id);
+        Contact Create(Contact contact);
+        Contact Update(Contact contact);
+        void Delete(int id);
+    }
+}
---
+++ ContactsWebApi/ContactsWebApi/Services/IContactService.cs
@@ -0,0 +1,17 @@
+﻿using ContactsWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsWebApi.Services
+{
+    public interface IContactService
+    {
+        List<Contact> GetContacts();
+        Contact GetContactById(int id);
+        Contact CreateContact(Contact contact);
+        Contact UpdateContact(int id, Contact contact);
+        void DeleteContact(int id);
+    }
+}
